1b41e8ade8174a59adaf8eb78cb7163932791143
Christoph Buchner <bilderbuchi@phononoia.at>
2014-07-10 23:00:50 +0200

    Teach get_merged_prs_since_tag to accept the tag as CLI argument.

--- get_merged_prs_since_tag.py
+++ get_merged_prs_since_tag.py
@@ -1,24 +1,45 @@
 #!/usr/bin/env python3
 
-"""Get a markdown-formatted list of PRs merged since a certain tag."""
+"""Get a markdown-formatted list of PRs merged since a certain tag.
+
+The tag name can be given as argument. If none was given, choose latest tag.
+"""
 
 import github_tools
+import sys
+
+if len(sys.argv) == 2:
+    tagname = sys.argv[1]
+else:
+    tagname = ''
 
 Repo = github_tools.get_repo()
-
-# TODO: implement getting date from repo tag.
-tagname = '0.8.1'
+print('Fetching tags...')
 tags = Repo.get_tags()
+tag_list = []
 for t in tags:
-    if t.name == tagname:
-        startdate = t.commit.commit.committer.date
-        print('Selected tag ' + tagname + ', date: ' + str(startdate))
+    tag_list.append({'name': t.name,
+                     'date': t.commit.commit.committer.date})
+
+if not tagname:
+    # no tagname was given, choose the youngest tag
+    tag = max(tag_list, key=lambda x: x['date'])
+else:
+    # find date for given tagname
+    for t in tag_list:
+        if tagname == t['name']:
+            tag = t
+            break
+    else:
+        # no appropriate tag was found
+        sys.exit('Tag name ' + tagname + ' not found, aborting!')
+print('Selected tag ' + tag['name'] + ', date: ' + str(tag['date']))
 
 print('Fetching data...\n\n')
 pulls = Repo.get_pulls('closed')
 results = []
 for p in pulls:
-    if (p.closed_at > startdate) and p.merged:
+    if (p.closed_at > tag['date']) and p.merged:
         results.append({'login': p.user.login,
                         'number': p.number,
                         'url': p.html_url,
